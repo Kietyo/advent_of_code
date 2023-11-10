2b47ae193ddfefd883f2dc5d9fa983b9be33bdce
Kiet <5054996+Kietyo@users.noreply.github.com>
2023-11-09 22:01:42 -0800

    20 day 11

--- src/test/kotlin/aoc_2020/20day11.kt
+++ src/test/kotlin/aoc_2020/20day11.kt
@@ -1,6 +1,8 @@
 package aoc_2020
 
+import utils.Direction
 import utils.MutableGrid
+import utils.PointWithData
 import utils.toGrid
 import kotlin.test.Test
 
@@ -32,6 +34,33 @@ internal class `20day11` {
         return newGrid
     }
 
+    private fun MutableGrid<Char>.getStrideAdjacents(x: Int, y: Int): List<PointWithData<Char>> {
+        return Direction.entries.mapNotNull {
+            getStrideFrom(x, y, it).firstOrNull {
+                it.data != '.'
+            }
+        }
+    }
+
+
+    private fun simulateRoundV2(grid: MutableGrid<Char>): MutableGrid<Char> {
+        val newGrid = grid.copy()
+        grid.getAllPoints(EMPTY).forEach {
+            val strideAdjacents = grid.getStrideAdjacents(it.x, it.y)
+            val numOccupiedAdjacents = strideAdjacents.filter { it.data == OCCUPIED }.size
+            if (numOccupiedAdjacents == 0) {
+                newGrid[it.x, it.y] = OCCUPIED
+            }
+        }
+        grid.getAllPoints(OCCUPIED).forEach {
+            val numOccupiedAdjacents = grid.getStrideAdjacents(it.x, it.y).filter { it.data == OCCUPIED }.size
+            if (numOccupiedAdjacents >= 5) {
+                newGrid[it.x, it.y] = EMPTY
+            }
+        }
+        return newGrid
+    }
+
     private fun part1Calculation(input: List<String>) {
         val converted = input.convertToDataObjectList()
 
@@ -49,7 +78,18 @@ internal class `20day11` {
 
     private fun part2Calculation(input: List<String>) {
         val converted = input.convertToDataObjectList()
-        println(converted)
+        var curr = converted
+        while (true) {
+            println("------------------------------")
+            println(curr)
+            val newGrid = simulateRoundV2(curr.copy())
+            if (curr.contentEquals(newGrid)) {
+                break
+            }
+            curr = newGrid
+        }
+
+        println(curr.getAllPoints(OCCUPIED).size)
     }
 
     @Test
@@ -76,3 +116,4 @@ internal class `20day11` {
         part2Calculation(input)
     }
 }
+
