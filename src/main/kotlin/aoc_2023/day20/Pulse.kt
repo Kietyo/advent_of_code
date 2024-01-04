43b2e86ad53ba11b3c72f3c4d836f7f09855d232
Kiet <5054996+Kietyo@users.noreply.github.com>
2024-01-03 21:31:05 -0800

    Renaming PACK_6_MASK -> PACK_7_MASK

--- src/main/kotlin/aoc_2023/day20/Pulse.kt
+++ src/main/kotlin/aoc_2023/day20/Pulse.kt
@@ -15,10 +15,10 @@ value class PulseI(val data: Int) {
     constructor(pulseType: PulseType, sender: Int, receiver: Int):
             this((((pulseType.ordinal shl MASK_LENGTH) or receiver) shl MASK_LENGTH) or sender)
     val pulseType: PulseType get() = if ((data ushr MASK_LENGTH_DOUBLE) == 1) PulseType.HIGH else PulseType.LOW
-    val sender: Int get() = (data and PACK_6_MASK)
-    val receiver: Int get() = ((data ushr MASK_LENGTH) and PACK_6_MASK)
+    val sender: Int get() = (data and PACK_7_MASK)
+    val receiver: Int get() = ((data ushr MASK_LENGTH) and PACK_7_MASK)
     companion object {
-        const val PACK_6_MASK = 0b1111111
+        const val PACK_7_MASK = 0b1111111
         const val MASK_LENGTH = 7
         const val MASK_LENGTH_DOUBLE = MASK_LENGTH * 2
     }
